ce60446e72afc390fa9493b220d94067cf3d3bab
niloofarkch <nikolahchi@gmail.com>
2023-04-05 22:03:37 -0500

    added effect and sound

---
+++ Assets/Scripts/playerInventory.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using static UnityEngine.EventSystems.EventTrigger;
+
+public class playerInventory : MonoBehaviour
+{
+    public int NumberOfFruits { get; private set; }
+    public UnityEvent<playerInventory> OnFruitCollected;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void FruitCollected()
+    {
+        NumberOfFruits++;
+        OnFruitCollected.Invoke(this);
+    }
+}
---
+++ Assets/Scripts/scoreUI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class scoreUI : MonoBehaviour
+{
+    private TextMeshProUGUI Score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Score = GetComponent<TextMeshProUGUI>();        ;
+    }
+    //update the UI for score
+    public void UpdateScore(playerInventory playerInventory)
+    {
+        Score.text = "Score : " + playerInventory.NumberOfFruits.ToString();
+    }
+   
+}
